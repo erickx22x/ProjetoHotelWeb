0af729388fd42ea8f722bfe277b67348b86ffe10
Érick Almeida <erickx22x@gmail.com>
2020-10-13 14:18:26 -0300

    Implementação das classes de modelo

---
+++ src/main/java/br/edu/iff/projetoHotel/model/Hotel.java
@@ -0,0 +1,95 @@
+package br.edu.iff.projetoHotel.model;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Objects;
+
+
+public class Hotel implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    private Long id;
+    private String nome;
+    private String cnfp;
+    
+    private Endereco endereco;
+    private List<Telefone> telefones;
+    private List<Quarto> quartos;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCnfp() {
+        return cnfp;
+    }
+
+    public void setCnfp(String cnfp) {
+        this.cnfp = cnfp;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public List<Telefone> getTelefones() {
+        return telefones;
+    }
+
+    public void setTelefones(List<Telefone> telefones) {
+        this.telefones = telefones;
+    }
+
+    public List<Quarto> getQuartos() {
+        return quartos;
+    }
+
+    public void setQuartos(List<Quarto> quartos) {
+        this.quartos = quartos;
+    }
+
+        
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 97 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Hotel other = (Hotel) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+}
